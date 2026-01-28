0e2a5e45dfee2a7682c575332bbc6b1c137641b9
Chandrashekar Rao Kuthyar <chandrashekarraokuthyar@Chandrashekars-MacBook-Air.local>
2026-01-28 19:01:58 +0530

    Name in reverse using substring

---
+++ J1/src/J7.java
@@ -0,0 +1,12 @@
+public class J7 {
+    public static void main(String[] args){
+        System.out.println("Reverse name");
+        String s1="ABCDE";
+        String s2="";
+
+        s2=s2+s1.substring(4,5);
+        s2=s2+s1.substring(3,4);
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
