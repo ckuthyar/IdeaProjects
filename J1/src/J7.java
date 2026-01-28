98dcec60934ba0adf5630ab6ee484ed373d9d02b
Chandrashekar Rao Kuthyar <ckuthyar@gmail.com>
2026-01-28 19:09:02 +0530

    used charAt function to fetch a particular character in a string

--- J1/src/J7.java
+++ J1/src/J7.java
@@ -1,11 +1,12 @@
 public class J7 {
     public static void main(String[] args){
         System.out.println("Reverse name");
-        String s1="ABCDE";
+        String s1="ABCDEFGHIJKLMN";
         String s2="";
-
-        s2=s2+s1.substring(4,5);
-        s2=s2+s1.substring(3,4);
+        int len1=s1.length();
+        for(int i=1;i<len1+1;i++){
+            s2=s2+s1.charAt(len1-i);
+        }
         System.out.println(s1);
         System.out.println(s2);
     }
