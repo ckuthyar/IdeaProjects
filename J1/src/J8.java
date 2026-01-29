3a688cf69206644d2ae01a3566b9017d8042cc1b
Chandrashekar Rao Kuthyar <ckuthyar@gmail.com>
2026-01-29 17:10:09 +0530

    remainder and exponential

---
+++ J1/src/J8.java
@@ -0,0 +1,12 @@
+public class J8 {
+    public static void main(String[] args){
+        int num1=8;
+        int num2=5;
+        int rem1=num1%num2;
+        double exp1=Math.pow(8,5);
+        System.out.println(rem1);
+        System.out.println(exp1);
+    }
+
+
+}
