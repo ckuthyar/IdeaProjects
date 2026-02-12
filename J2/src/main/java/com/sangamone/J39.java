303083e9242c8864cb381f2d38f7bf1ae5861bba
Chandrashekar Rao Kuthyar <ckuthyar@gmail.com>
2026-02-13 03:40:41 +0530

    class MyCar constructor with 2 parameters modelYear, modelName so that the objects can be initializd when instantiated

---
+++ J2/src/main/java/com/sangamone/J39.java
@@ -0,0 +1,20 @@
+package com.sangamone;
+class MyCar{
+    int modelYear;
+    String modelName;
+    public MyCar(int year, String name){//constructor has same name as class
+        modelYear=year;
+        modelName=name;
+    }
+}
+public class J39 {
+    public static void main(String[] args){
+        System.out.println("Java - Constructor"); //initialise object when created
+        MyCar c1= new MyCar(1995, "Maruti 800");
+        MyCar c2= new MyCar(2001, "Maruti Zen");
+        MyCar c3 = new MyCar(2010, "Maruti Swift");
+        MyCar c4 = new MyCar(2024, "Maruti Celerio");
+
+        System.out.println(c1.modelName + " was purchased in "+c1.modelYear);
+    }
+}
