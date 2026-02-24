5a0236cabc31959de0e53a2325b60eba5d195ef3
Chandrashekar Rao Kuthyar <ckuthyar@gmail.com>
2026-02-24 08:20:07 +0530

    Mathematical tables keyboard input, file in1.txt, in2.txt, out1.txt

--- J3/src/main/java/com/sangamone/J2.java
+++ J3/src/main/java/com/sangamone/J2.java
@@ -13,14 +13,33 @@
 //        20 * 10 =200     Put a line break
 
 package com.sangamone;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 public class J2 {
-    public static void main(String[] args){
+    public static void main(String[] args)throws FileNotFoundException, IOException{
         System.out.println("Mathematical Tables");
-        for(int j=3;j<6;j++){
+        File f1=new File("in2.txt");
+        FileWriter fw1=new FileWriter("out1.txt");
+        Scanner sc1=new Scanner(f1);
+        String info1="";
+        String s1="";
+        s1=sc1.nextLine();
+        String[] arr1=s1.split(",");
+        int num1=Integer.parseInt(arr1[0]);
+        int num2=Integer.parseInt(arr1[1]);
+        for(int j=num1;j<num2+1;j++){
             for(int i=1;i<11;i++){
-                System.out.println(j + "*" + i + "=" + j*i);
+                info1=(j + "*" + i + "=" + j*i);
+                System.out.println(info1);
+                fw1.write(info1);
+                fw1.write("\n");
             }
+            fw1.write("\n");
             System.out.println();
         }
+        fw1.close();
     }
 }
